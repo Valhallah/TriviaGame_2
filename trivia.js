f4cf3c4bf1bd2fae928fa2f7b6193321770917ed
ValHall <Valynhall@gmail.com>
2017-03-03 21:19:17 -0500

    html updates - jquery updates with comments for clarity

--- trivia.js
+++ trivia.js
@@ -52,24 +52,34 @@ $(document).ready(function () {
     // When question is answered.
     $('input[type=checkbox][name=answer]').change(function() {
         $('#questionDiv').hide();
+        //if question is not equal to the answer stored
         if (this.value != questionAnswer[currentQuestion].answer) {
+            //show wrong message
             $("#wrongDisplay").show();
+            //fade wrong message over 3 seconds
             $("#wrongDisplay").fadeOut(3000);
             wrongAns++;
         }
+        //if question is equal to the answer stored
         else if (this.value == questionAnswer[currentQuestion].answer) {
+            //show right message
             $("#correctDisplay").show();
+            //fade right message over 3 seconds
             $("#correctDisplay").fadeOut(3000);
             rightAns++;
             score += 10;
         }
         
+        //increment question
         currentQuestion++;
         
+        //if current question is greater than or equal to the amount of questions 
         if (currentQuestion >= questionAnswer.length) {
+            //display score
             $('#resultsDisplay').text("Congrats! You got " + rightAns + " of 7 questions correct!");
             $('#resultsDisplay').show();
         } else {
+            // otherwise display next question
             $('#questionLabel').text(questionAnswer[currentQuestion].question);
             $('#scoreBox').text(score);
             $('#questionDiv').fadeIn(1000);
@@ -187,10 +197,7 @@ $(document).ready(function () {
         }*/
         
         //displays score
-        $('#scoreBox').text(score);
-        //displays results at end
-        $('#resultsDisplay').text("Congrats! You got " + rightAns + " of 7 questions correct!");
-        $('#resultsDisplay').show();
+        
     });
 	
  
